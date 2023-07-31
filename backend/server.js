7511256695dc5365c78b7f61482bd80d7821792f
Samuel2208 <108751960+Samuel2208@users.noreply.github.com>
2023-07-30 22:03:22 -0500

    fixed log in

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const fsPromises = require("fs").promises;
 //const fs = require("fs");
 const todoDBName = "tododb";
+// const useCloudant = false;
 const useCloudant = true;
 
 
@@ -19,8 +20,23 @@ if (useCloudant)
     initDB();
 }
 
+//week 7:
+// AUTH--
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+// AUTH--
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000'
+}));
+
+// app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 
 app.listen(port, () => console.log("Backend server live on " + port));
@@ -139,6 +155,24 @@ async function searchItems (request, response) {
     }
 };
 
+// AUTH--
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const upsertSucceeded = upsertUser(username, password)
+  res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('user');
+  res.end();
+});
 
 // Add initDB function here
 async function initDB ()
@@ -164,41 +198,3 @@ async function initDB ()
 
   }
 };
-
-//week 7:
-const basicAuth = require("express-basic-auth");
-var { authenticator, upsertUser, cookieAuth } = require("./authentication");
-const auth = basicAuth({
-    authorizer: authenticator
-});
-const cookieParser = require("cookie-parser");
-app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
-
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:3000'
-}));
-
-app.get("/authenticate", auth, (req, res) => {
-  console.log(`user logging in: ${req.auth.user}`);
-  res.cookie('user', req.auth.user, { signed: true });
-  res.sendStatus(200);
-});
-
-app.post("/users", (req, res) => {
-  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-  const upsertSucceeded = upsertUser(username, password)
-  res.sendStatus(upsertSucceeded ? 200 : 401);
-});
-
-app.get("/logout", (req, res) => {
-  res.clearCookie('user');
-  res.end();
-});
-
-app.post("/items", cookieAuth, addItem);
-
-app.get("/items", cookieAuth, getItems);
-
-app.get("/items/search", cookieAuth, searchItems);
